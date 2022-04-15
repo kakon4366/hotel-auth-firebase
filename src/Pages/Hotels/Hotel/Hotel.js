a3d90b942e97e66bb4b36a516cb0887ab1ab56e2
kakon4366 <kakonbarman4366@gmail.com>
2022-04-15 13:51:42 +0600

    Hotel image fix

--- src/Pages/Hotels/Hotel/Hotel.js
+++ src/Pages/Hotels/Hotel/Hotel.js
@@ -5,7 +5,7 @@ const Hotel = ({ hotel }) => {
 	const { img, name } = hotel;
 	return (
 		<div className="hotel">
-			<img width="100%" src={img} alt="" />
+			<img height="100%" width="100%" src={img} alt="" />
 			<div className="hotel-name">
 				<h3>{name}</h3>
 			</div>
