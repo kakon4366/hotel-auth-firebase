073faad6144487ef328ead4ac4bf1e8b67351ec4
kakon4366 <kakonbarman4366@gmail.com>
2022-04-15 11:04:34 +0600

    Not Found page component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home";
+import NotFound from "./Pages/NotFound/NotFound";
 import Footer from "./Pages/Shared/Footer/Footer";
 import Header from "./Pages/Shared/Header/Header";
 
@@ -11,6 +12,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />}></Route>
 				<Route path="/home" element={<Home />}></Route>
+				<Route path="*" element={<NotFound />}></Route>
 			</Routes>
 			<Footer></Footer>
 		</div>
---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NotFound.css";
+
+const NotFound = () => {
+	return (
+		<div className="not-found">
+			<h2>This Page is Not Available!!!</h2>
+			<span>404</span>
+		</div>
+	);
+};
+
+export default NotFound;
