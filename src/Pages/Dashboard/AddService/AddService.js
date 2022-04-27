0049a41a353a9ce371acfca66c5977b1682d66a0
kakon4366 <kakonbarman4366@gmail.com>
2022-04-27 19:54:28 +0600

    Services add post method added

--- src/Pages/Dashboard/AddService/AddService.js
+++ src/Pages/Dashboard/AddService/AddService.js
@@ -1,10 +1,28 @@
 import React from "react";
 import "./AddServices.css";
 const AddService = () => {
+	const handleAddService = (e) => {
+		e.preventDefault();
+		const name = e.target.name.value;
+		const price = e.target.price.value;
+		const img = e.target.photoUrl.value;
+
+		const service = { name, price, img };
+
+		fetch("", {
+			method: "POST",
+			headers: {
+				"content-type": "application/json",
+			},
+			body: JSON.stringify(service),
+		})
+			.then((res) => res.json)
+			.then((data) => console.log(data));
+	};
 	return (
 		<div>
 			<h2>Add Service</h2>
-			<div className="service-field">
+			<form onSubmit={handleAddService} className="service-field">
 				<label htmlFor="name">Name</label>
 				<input type="text" name="name" />
 				<label htmlFor="price">Price</label>
@@ -12,7 +30,7 @@ const AddService = () => {
 				<label htmlFor="photoUrl">Photo URL</label>
 				<input type="text" name="photoUrl" />
 				<input type="submit" value="Add Services" />
-			</div>
+			</form>
 		</div>
 	);
 };
