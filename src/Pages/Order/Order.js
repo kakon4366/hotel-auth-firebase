504e180554042a327b63cd2b158240adf05c9c8b
kakon4366 <kakonbarman4366@gmail.com>
2022-04-28 13:09:47 +0600

    Show all orders

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -10,6 +10,8 @@ const Order = () => {
 	const [rooms] = useServices();
 	const [user] = useAuthState(auth);
 	const [bookingRoom, setBookingRoom] = useState("");
+	const [orders, setOrders] = useState(0);
+	const [isOrder, setIsOrder] = useState(false);
 
 	const { serviceId } = useParams();
 	const bookingRooms = rooms.find((room) => room._id === serviceId);
@@ -17,6 +19,14 @@ const Order = () => {
 	useEffect(() => {
 		setBookingRoom(bookingRooms?.name);
 	}, [bookingRooms]);
+
+	useEffect(() => {
+		fetch("http://localhost:5000/orderList")
+			.then((res) => res.json())
+			.then((data) => {
+				setOrders(data);
+			});
+	}, [isOrder]);
 
 	const handleOrder = (e) => {
 		e.preventDefault();
@@ -38,47 +48,59 @@ const Order = () => {
 			.then((res) => res.json())
 			.then((data) => {
 				toast.success(data.message);
+				setIsOrder(!isOrder);
 				e.target.reset();
 			});
 	};
 
 	return (
 		<div className="container order-area">
-			<h2>Place Your Order</h2>
-			<form onSubmit={handleOrder} className="order-form">
-				<input
-					type="text"
-					value={user?.displayName}
-					name="name"
-					placeholder="Your Name"
-					required
-				/>
-				<input
-					type="email"
-					value={user?.email}
-					name="email"
-					placeholder="Email Address"
-					readOnly
-					disabled
-					required
-				/>
-				<input
-					type="text"
-					value={bookingRoom}
-					onChange={(e) => setBookingRoom(e.target.value)}
-					name="room"
-					placeholder="Your Booking Room"
-					required
-				/>
-				<input type="text" name="address" placeholder="Address" required />
-				<input
-					type="text"
-					name="phone"
-					placeholder="Phone Numbar"
-					required
-				/>
-				<input type="submit" value="Order Now" />
-			</form>
+			<div>
+				<h2>Place Your Order</h2>
+				<form onSubmit={handleOrder} className="order-form">
+					<input
+						type="text"
+						value={user?.displayName}
+						name="name"
+						placeholder="Your Name"
+						required
+					/>
+					<input
+						type="email"
+						value={user?.email}
+						name="email"
+						placeholder="Email Address"
+						readOnly
+						disabled
+						required
+					/>
+					<input
+						type="text"
+						value={bookingRoom}
+						onChange={(e) => setBookingRoom(e.target.value)}
+						name="room"
+						placeholder="Your Booking Room"
+						required
+					/>
+					<input
+						type="text"
+						name="address"
+						placeholder="Address"
+						required
+					/>
+					<input
+						type="text"
+						name="phone"
+						placeholder="Phone Numbar"
+						required
+					/>
+					<input type="submit" value="Order Now" />
+				</form>
+			</div>
+			{/* order show */}
+			<div>
+				<h2>Your Total Order: {orders.length}</h2>
+			</div>
 		</div>
 	);
 };
